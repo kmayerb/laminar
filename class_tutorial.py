3d8b95ff8afe010c5d9b1798735949d76f080394
Dane <danegellerup@uwalumni.com>
2019-11-08 09:31:34 -0800

    Adding example code to class_tutorial.

--- class_tutorial.py
+++ class_tutorial.py
@@ -10,6 +10,34 @@ Created on Thu Nov  7 23:14:01 2019
 from laminar import laminar
 from laminar import laminar_examples as le
 
+
+# Module Usage
+
+# iter_flow: one iterable to break up; broken up into n chunks, where n = cores
+
+# le.single_total reuturns sum, but can take odd=True to only sum odd numbers.
+result = laminar.iter_flow(le.single_total, le.laminar_df['Col1'])
+
+result = laminar.iter_flow(le.single_total, le.laminar_df['Col1'], True)
+result = laminar.iter_flow(le.single_total, le.laminar_df['Col1'], odd=True)
+
+# le.multi_tally returns count of rows that sum to greater than 25
+result = laminar.iter_flow(le.multi_tally, le.laminar_df)
+
+# list_flow: list of iterables; can contain more iterables than cores; processes
+#            will pull from the queue until it is empty.
+
+# create a list of pd.Series corresponding to laminar_df columns
+columns_list = [le.laminar_df[col] for col in le.laminar_df.columns]
+result = laminar.list_flow(le.single_total, columns_list, odd=True)
+
+# Make a list of three copies of le.laminar_df
+data_frames_list = [le.laminar_df]*3
+result = laminar.list_flow(le.multi_tally, data_frames_list)
+
+
+# Class Usage
+
 my_lam = laminar.Laminar()
 
 my_lam.show_processes()
